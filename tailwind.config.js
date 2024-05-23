c49d2712f74e2dfc11d86987d836164eb1d5f14e
Mirko Bozzetto <mirkouch@gmail.com>
2024-05-23 10:02:11 +0200

    init

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        nutmeg: "hsl(14, 45%, 36%)",
+        darkrapberry: "hsl(332, 51%, 32%)",
+        white: "hsl(0, 0%, 100%)",
+        roseWhite: "hsl(330, 100%, 98%)",
+        eggshell: "hsl(30, 54%, 90%)",
+        lightGrey: "hsl(30, 18%, 87%)",
+        wengeBrown: "hsl(30, 10%, 34%)",
+        darkCharcoal: "hsl(24, 5%, 18%)",
+      },
+      fontFamily: {
+        young: ["Young Serif", "serif"],
+        outfit: ["Outfit", "sans-serif"],
+      },
+      fontWeight: {
+        regular: 400,
+        medium: 600,
+        bold: 700,
+      },
+    },
+  },
+  plugins: [],
+};
